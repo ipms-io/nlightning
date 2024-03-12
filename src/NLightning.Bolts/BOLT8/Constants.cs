2c9615aa4cd7d1ec877a874a8a3b59f35c1e4497
Níckolas Goline <nickolas.goline+github@gmail.com>
2024-03-12 09:31:16 -0300

    implement noise

---
+++ src/NLightning.Bolts/BOLT8/Constants.cs
@@ -0,0 +1,7 @@
+namespace NLightning.Bolts.BOLT8;
+
+public static class Constants
+{
+    public const string PROTOCOL_NAME = "Noise_XK_secp256k1_ChaChaPoly_SHA256";
+    public const string PROLOGUE = "lightning";
+}
