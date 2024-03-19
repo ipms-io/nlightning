e54ba50730c1b71dd736e17b39a3e8fb8744d40b
Níckolas Goline <nickolas.goline+github@gmail.com>
2024-03-19 10:09:18 -0300

    fix hex conversion on TestUtils

--- test/NLightning.Bolts.Tests/BOLT8/Utils/TestUtils.cs
+++ test/NLightning.Bolts.Tests/BOLT8/Utils/TestUtils.cs
@@ -4,12 +4,6 @@ public static class TestUtils
 {
     public static byte[] GetBytes(string hex)
     {
-        hex = hex.Replace("0x", string.Empty);
-        var bytes = new byte[hex.Length / 2];
-        for (var i = 0; i < bytes.Length; i++)
-        {
-            bytes[i] = Convert.ToByte(hex.Substring(i * 2, 2), 16);
-        }
-        return bytes;
+        return Convert.FromHexString(hex.Replace("0x", string.Empty));
     }
 }
