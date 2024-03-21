2b2dc6a5d280f0ecedcbd070d8ff9a675e84b29a
Richard Safier <richardj@safier.com>
2024-03-20 21:23:58 -0400

    cleanup

--- test/NLightning.Bolts.Tests/Docker/DockerTest.cs
+++ test/NLightning.Bolts.Tests/Docker/DockerTest.cs
@@ -1,6 +1,4 @@
-using System.Collections;
 using System.Diagnostics;
-using System.Net.Mime;
 using Docker.DotNet;
 using Docker.DotNet.Models;
 using NBitcoin;
@@ -8,44 +6,38 @@ using NBitcoin.RPC;
 using NLightning.Bolts.Tests.Utils;
 using ServiceStack.Text;
 using Xunit.Abstractions;
-using Xunit.Sdk;
 
 namespace NLightning.Bolts.Tests.Docker;
- 
+
 public class DockerFixture : IDisposable
 {
     public readonly DockerClient Client = new DockerClientConfiguration().CreateClient();
-    
-    public DockerFixture()
-    {
-        //We use this image for tests
-        Client.PullImageAndWaitForCompleted("redis", "5.0").Wait();
-    }
+
     public void Dispose()
     {
         Client.Dispose();
     }
 }
 
-public class DockerTest: IClassFixture<DockerFixture> 
+public class DockerTest : IClassFixture<DockerFixture>
 {
+    private readonly DockerClient _client;
     private readonly Random _random = new();
 
     private bool _sampleImagePulled;
-    private readonly DockerClient _client;
- 
-    public DockerTest(DockerFixture f,ITestOutputHelper output)
+
+    public DockerTest(DockerFixture f, ITestOutputHelper output)
     {
         _client = f.Client;
         Console.SetOut(new TestOutputWriter(output));
     }
-    
+
     private void RemoveContainer(string name)
     {
         try
         {
             _client.Containers.RemoveContainerAsync(name,
-                new ContainerRemoveParameters {Force = true, RemoveVolumes = true});
+                new ContainerRemoveParameters { Force = true, RemoveVolumes = true }).Wait();
         }
         catch
         {
@@ -56,6 +48,7 @@ public class DockerTest: IClassFixture<DockerFixture>
     [Fact]
     public async Task SetupBitcoinNode()
     {
+        RemoveContainer("btc_0"); //cleanup prior crud
         var image = "polarlightning/bitcoind";
         var tag = "26.0";
         await _client.PullImageAndWaitForCompleted(image, tag);
@@ -64,7 +57,7 @@ public class DockerTest: IClassFixture<DockerFixture>
             Image = $"{image}:{tag}",
             HostConfig = new HostConfig
             {
-                NetworkMode = $"bridge"
+                NetworkMode = "bridge"
             },
             Name = "btc_0",
             Hostname = "btc_0",
@@ -72,7 +65,7 @@ public class DockerTest: IClassFixture<DockerFixture>
             {
                 @"bitcoind",
                 @"-server=1",
-                $"-regtest=1",
+                "-regtest=1",
                 @"-rpcauth=bitcoin:c8c8b9740a470454255b7a38d4f38a52$e8530d1c739a3bb0ec6e9513290def11651afbfd2b979f38c16ec2cf76cf348a",
                 @"-debug=1",
                 @"-zmqpubrawblock=tcp://0.0.0.0:28334",
@@ -91,7 +84,7 @@ public class DockerTest: IClassFixture<DockerFixture>
                 @"-fallbackfee=0.0002"
             }
         });
-        var DockerContainerId = nodeContainer.ID;
+        var dockerContainerId = nodeContainer.ID;
         var success =
             await _client.Containers.StartContainerAsync(nodeContainer.ID, new ContainerStartParameters());
         await Task.Delay(500);
@@ -101,17 +94,16 @@ public class DockerTest: IClassFixture<DockerFixture>
         var bitcoinRpcClient = new RPCClient("bitcoin:bitcoin",
             bitcoinNode.NetworkSettings.Networks.First().Value.IPAddress, Bitcoin.Instance.Regtest);
         bitcoinRpcClient.HttpClient.Timeout = TimeSpan.FromMilliseconds(10000); //10s
-        await bitcoinRpcClient.CreateWalletAsync("default", new CreateWalletOptions {LoadOnStartup = true});
+        await bitcoinRpcClient.CreateWalletAsync("default", new CreateWalletOptions { LoadOnStartup = true });
         var utxos = await bitcoinRpcClient.GenerateAsync(200);
 
         var balance = await bitcoinRpcClient.GetBalanceAsync();
-        $"Running on Container Id: {DockerContainerId}".Print(); 
+        $"Running on Container Id: {dockerContainerId}".Print();
         $"Bitcoin Balance: {balance.Satoshi / 1e8} BTC".Print();
         RemoveContainer("btc_0");
     }
 
     [Fact]
-   // [Category("Docker")]
     public async Task ListContainer()
     {
         IList<ContainerListResponse> containers = await _client.Containers.ListContainersAsync(
@@ -119,15 +111,12 @@ public class DockerTest: IClassFixture<DockerFixture>
             {
                 Limit = 10
             });
-        // Assert.That(containers.Any());
     }
 
-
     [Fact]
-//    [Category("Docker")]
     public async Task MakeContainer()
     {
-        await PullRedis5(); 
+        await PullRedis5();
         var x = await _client.Containers.CreateContainerAsync(new CreateContainerParameters
         {
             Image = "redis:5.0",
@@ -139,12 +128,10 @@ public class DockerTest: IClassFixture<DockerFixture>
                 }
             }
         });
-        await _client.Containers.RemoveContainerAsync(x.ID,
-            new ContainerRemoveParameters {RemoveVolumes = true, Force = true});
+        RemoveContainer(x.ID);
     }
 
     [Fact]
- //   [Category("Docker")]
     public async Task Docker_PullImage()
     {
         await PullRedis5();
@@ -153,14 +140,16 @@ public class DockerTest: IClassFixture<DockerFixture>
     private async Task PullRedis5()
     {
         if (_sampleImagePulled)
+        {
             return;
+        }
+
         await _client.PullImageAndWaitForCompleted("redis", "5.0");
         _sampleImagePulled = true;
     }
 
 
     [Fact]
-//    [Category("Docker")]
     public async Task CreateDestroyNetwork()
     {
         var randomString = GetRandomHexString();
@@ -181,42 +170,22 @@ public class DockerTest: IClassFixture<DockerFixture>
     }
 
 
-   //  [Fact]
-   // // [Category("Docker")]
-   //  public async Task BuildDockerImage()
-   //  {
-   //      await _client.CreateDockerImageFromPath("./../../../../Docker/", new List<string> {"polar_lnd_0_16_1_test"});
-   //  }
+    //  [Fact]
+    // // [Category("Docker")]
+    //  public async Task BuildDockerImage()
+    //  {
+    //      await _client.CreateDockerImageFromPath("./../../../../Docker/", new List<string> {"polar_lnd_0_16_1_test"});
+    //  }
 
 
     [Fact]
-   // [Category("Docker")]
     public async Task DetectGitlabRunner()
     {
         var result = await _client.GetGitlabRunnerNetworkId();
         Debug.Print(result);
     }
 
-    // public string GetIPAddress()
-    // {
-    //     string IPAddress = "";
-    //     IPHostEntry Host = default(IPHostEntry);
-    //     string Hostname = null;
-    //     Hostname = System.Environment.MachineName;
-    //     Host = Dns.GetHostEntry(Hostname);
-    //     foreach (IPAddress IP in Host.AddressList)
-    //     {
-    //         if (IP.AddressFamily == System.Net.Sockets.AddressFamily.InterNetwork)
-    //         {
-    //             IPAddress = Convert.ToString(IP);
-    //         }
-    //     }
-    //
-    //     return IPAddress;
-    // }
-
     [Fact]
-    // [Category("Docker")]
     public async Task ComposeAndDestroyCluster()
     {
         await PullRedis5();
@@ -250,9 +219,9 @@ public class DockerTest: IClassFixture<DockerFixture>
         Assert.Empty(bob.Warnings);
 
         await _client.Containers.RemoveContainerAsync(alice.ID,
-            new ContainerRemoveParameters {RemoveVolumes = true, Force = true});
+            new ContainerRemoveParameters { RemoveVolumes = true, Force = true });
         await _client.Containers.RemoveContainerAsync(bob.ID,
-            new ContainerRemoveParameters {RemoveVolumes = true, Force = true});
+            new ContainerRemoveParameters { RemoveVolumes = true, Force = true });
         await _client.Networks.DeleteNetworkAsync(networksCreateResponse.ID);
     }
 
