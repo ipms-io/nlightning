189f0d89c3a8505ffece83269989e0e45431e304
Richard Safier <richardj@safier.com>
2024-03-20 21:10:39 -0400

    unit tests can output to console like nunit. bitcoin core node spinup and mine

--- test/NLightning.Bolts.Tests/Docker/DockerTest.cs
+++ test/NLightning.Bolts.Tests/Docker/DockerTest.cs
@@ -1,7 +1,15 @@
 using System.Collections;
 using System.Diagnostics;
+using System.Net.Mime;
 using Docker.DotNet;
 using Docker.DotNet.Models;
+using NBitcoin;
+using NBitcoin.RPC;
+using NLightning.Bolts.Tests.Helper;
+using ServiceStack.Text;
+using Xunit.Abstractions;
+using Xunit.Sdk;
+
 namespace NLightning.Bolts.Tests.Docker;
  
 public class DockerFixture : IDisposable
@@ -18,19 +26,21 @@ public class DockerFixture : IDisposable
         Client.Dispose();
     }
 }
+
 public class DockerTest: IClassFixture<DockerFixture> 
 {
     private readonly Random _random = new();
 
     private bool _sampleImagePulled;
     private readonly DockerClient _client;
-
-    public DockerTest(DockerFixture f)
+ 
+    public DockerTest(DockerFixture f,ITestOutputHelper output)
     {
         _client = f.Client;
+        Console.SetOut(new TestOutputWriter(output));
     }
     
-    private void ResetContainer(string name)
+    private void RemoveContainer(string name)
     {
         try
         {
@@ -41,6 +51,63 @@ public class DockerTest: IClassFixture<DockerFixture>
         {
             // ignored
         }
+    }
+
+    [Fact]
+    public async Task SetupBitcoinNode()
+    {
+        var image = "polarlightning/bitcoind";
+        var tag = "26.0";
+        await _client.PullImageAndWaitForCompleted(image, tag);
+        var nodeContainer = await _client.Containers.CreateContainerAsync(new CreateContainerParameters
+        {
+            Image = $"{image}:{tag}",
+            HostConfig = new HostConfig
+            {
+                NetworkMode = $"bridge"
+            },
+            Name = "btc_0",
+            Hostname = "btc_0",
+            Cmd = new List<string>
+            {
+                @"bitcoind",
+                @"-server=1",
+                $"-regtest=1",
+                @"-rpcauth=bitcoin:c8c8b9740a470454255b7a38d4f38a52$e8530d1c739a3bb0ec6e9513290def11651afbfd2b979f38c16ec2cf76cf348a",
+                @"-debug=1",
+                @"-zmqpubrawblock=tcp://0.0.0.0:28334",
+                @"-zmqpubrawtx=tcp://0.0.0.0:28335",
+                @"-zmqpubhashblock=tcp://0.0.0.0:28336",
+                @"-txindex=1",
+                @"-dnsseed=0",
+                @"-upnp=0",
+                @"-rpcbind=0.0.0.0",
+                @"-rpcallowip=0.0.0.0/0",
+                @"-rpcport=18443",
+                @"-rest",
+                @"-rpcworkqueue=1024",
+                @"-listen=1",
+                @"-listenonion=0",
+                @"-fallbackfee=0.0002"
+            }
+        });
+        var DockerContainerId = nodeContainer.ID;
+        var success =
+            await _client.Containers.StartContainerAsync(nodeContainer.ID, new ContainerStartParameters());
+        await Task.Delay(500);
+        //Setup wallet and basic funds
+        var listContainers = await _client.Containers.ListContainersAsync(new ContainersListParameters());
+        var bitcoinNode = listContainers.First(x => x.ID == nodeContainer.ID);
+        var bitcoinRpcClient = new RPCClient("bitcoin:bitcoin",
+            bitcoinNode.NetworkSettings.Networks.First().Value.IPAddress, Bitcoin.Instance.Regtest);
+        bitcoinRpcClient.HttpClient.Timeout = TimeSpan.FromMilliseconds(10000); //10s
+        await bitcoinRpcClient.CreateWalletAsync("default", new CreateWalletOptions {LoadOnStartup = true});
+        var utxos = await bitcoinRpcClient.GenerateAsync(200);
+
+        var balance = await bitcoinRpcClient.GetBalanceAsync();
+        $"Running on Container Id: {DockerContainerId}".Print(); 
+        $"Bitcoin Balance: {balance.Satoshi / 1e8} BTC".Print();
+        RemoveContainer("btc_0");
     }
 
     [Fact]
