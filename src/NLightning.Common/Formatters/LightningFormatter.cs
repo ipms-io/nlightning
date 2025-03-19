848be521e4f06be06c832d3a9bc935b1e52db4b3
Níckolas Goline <nickolas.goline+github@gmail.com>
2025-03-19 00:26:41 -0300

    add LightningMoney to deal with amounts in millisats

---
+++ src/NLightning.Common/Formatters/LightningFormatter.cs
@@ -0,0 +1,41 @@
+using System.Globalization;
+
+namespace NLightning.Common.Formatters;
+
+using Enums;
+
+public class LightningFormatter : IFormatProvider, ICustomFormatter
+{
+    public static readonly LightningFormatter FORMATTER = new LightningFormatter();
+
+    public object? GetFormat(Type formatType)
+    {
+        return formatType == typeof(ICustomFormatter) ? this : null;
+    }
+
+    public string Format(string? format, object? arg, IFormatProvider? formatProvider)
+    {
+        if (!Equals(formatProvider) || string.IsNullOrWhiteSpace(format))
+        {
+            return string.Empty;
+        }
+        
+        var i = 0;
+        if (int.TryParse(format.AsSpan(i, 1), out var decPos))
+        {
+            i++;
+        }
+        var unit = format[i];
+        var unitToUseInCalc = unit switch
+        {
+            'B' => LightningMoneyUnit.BTC,
+            _ => LightningMoneyUnit.BTC
+        };
+        var val = Convert.ToDecimal(arg, CultureInfo.InvariantCulture) / (ulong)unitToUseInCalc;
+        var zeros = new string('0', decPos);
+        var rest = new string('#', 11 - decPos);
+
+        var fmt = "{0:0" + (decPos > 0 ? "." + zeros + rest : string.Empty) + "}";
+        return string.Format(CultureInfo.InvariantCulture, fmt, val);
+    }
+}
