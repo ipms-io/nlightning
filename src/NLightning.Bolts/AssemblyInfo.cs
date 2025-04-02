bf5c40f6083c06a4f8f096a808a53d9f89c0f7fd
thgOyo <thiagooyo@gmail.com>
2025-04-02 01:15:33 -0300

    test: add ActOneFuzzer for fuzzing act one messages

--- src/NLightning.Bolts/AssemblyInfo.cs
+++ src/NLightning.Bolts/AssemblyInfo.cs
@@ -2,3 +2,5 @@ using System.Runtime.CompilerServices;
 
 [assembly: InternalsVisibleTo("NLightning.Bolts.Tests")]
 [assembly: InternalsVisibleTo("DynamicProxyGenAssembly2")]
+[assembly: InternalsVisibleTo("ActOneFuzzer")]
+[assembly: InternalsVisibleTo("ActTwoFuzzer")]
