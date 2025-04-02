4d87ecb257b864b98a597e734988c02a7a4f514e
thgOyo <thiagooyo@gmail.com>
2025-04-02 10:03:04 -0300

    test: add ActTwoFuzzer for fuzzing act two messages

---
+++ test/FuzzTests/NLightning.Bolts.BOLT8/ActTwoFuzzer/Program.cs
@@ -0,0 +1,46 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+using NLightning.Bolts.BOLT8.Constants;
+using NLightning.Bolts.BOLT8.States;
+using NLightning.Bolts.Tests.BOLT8.Mock;
+using NLightning.Bolts.Tests.BOLT8.Utils;
+using SharpFuzz;
+
+namespace ActTwoFuzzer;
+
+internal abstract class ActTwoFuzzer
+{
+    private static void Main(string[] args)
+    {
+        Fuzzer.OutOfProcess.Run(stream =>
+        {
+            //pwsh ../../fuzz.ps1 ActTwoFuzzer.csproj -i Testcases
+            try
+            {
+                using var memory = new MemoryStream();
+                stream.CopyTo(memory);
+                var data = memory.ToArray().AsSpan();
+        
+                var initiator = new HandshakeState(
+                    true,
+                    InitiatorValidKeysUtil.LocalStaticPrivateKey,
+                    InitiatorValidKeysUtil.RemoteStaticPublicKey,
+                    new FakeFixedKeyDh(InitiatorValidKeysUtil.EphemeralPrivateKey)
+                );
+        
+                var buffer = new byte[ProtocolConstants.MAX_MESSAGE_LENGTH];
+        
+                initiator.WriteMessage(Encoding.ASCII.GetBytes(string.Empty), buffer);
+        
+                initiator.ReadMessage(data, buffer);
+            }
+            catch (ObjectDisposedException) { }
+            catch (InvalidOperationException) { }
+            catch (ArgumentException) { }
+            catch (CryptographicException) { }
+            catch (FormatException) { } //Crash found: System.FormatException: Invalid public key at NBitcoin.PubKey..ctor(ReadOnlySpan`1 bytes)
+
+        });
+    }
+}
+
