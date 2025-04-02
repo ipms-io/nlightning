16c39f2ce238f0d672a812f624ef32c52f0d3827
thgOyo <thiagooyo@gmail.com>
2025-04-02 00:52:41 -0300

    test: add round-trip fuzzer for BOLT11 invoice encoding/decoding

---
+++ test/FuzzTests/NLightning.Bolts.BOLT11/InvoiceRoundTripFuzzer/Program.cs
@@ -0,0 +1,33 @@
+﻿using NLightning.Bolts.BOLT11;
+using NLightning.Bolts.Exceptions;
+using SharpFuzz;
+
+namespace InvoiceRoundTripFuzzer;
+
+internal abstract class InvoiceRoundTripFuzzer
+{
+    private static void Main()
+    {
+        Fuzzer.OutOfProcess.Run(stream =>
+        {
+            //pwsh ../../fuzz.ps1 InvoiceRoundTripFuzzer.csproj -i Testcases
+            try
+            {
+                using var reader = new StreamReader(stream);
+
+                var invoiceData = reader.ReadToEnd();
+
+                var invoice = Invoice.Decode(invoiceData);
+
+                var encodedInvoice = invoice.Encode();
+
+                if (!invoiceData.Equals(encodedInvoice, StringComparison.OrdinalIgnoreCase))
+                {
+                    throw new Exception("Invoice mismatch");
+                }
+
+            }
+            catch (InvoiceSerializationException) { }
+        });
+    }
+}
