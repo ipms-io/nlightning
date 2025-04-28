705c395f1447721abe6341eaf6b7cd4f44ff1e6a
Níckolas Goline <nickolas.goline+github@gmail.com>
2025-04-28 13:56:19 -0300

    bump bolt11 version
    Signed-off-by: Níckolas Goline <nickolas.goline+github@gmail.com>

--- samples/Bolt11.Decoder.Blazor/Program.cs
+++ samples/Bolt11.Decoder.Blazor/Program.cs
@@ -2,12 +2,20 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using NLightning.Common.Crypto.Providers.JS;
 
-var builder = WebAssemblyHostBuilder.CreateDefault(args);
-builder.RootComponents.Add<App>("#app");
-builder.RootComponents.Add<HeadOutlet>("head::after");
+namespace Bolt11.Decoder.Blazor;
 
-builder.Services.AddScoped(_ => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+public class Program
+{
+    public static async Task Main(string[] args)
+    {
+        var builder = WebAssemblyHostBuilder.CreateDefault(args);
+        builder.RootComponents.Add<App>("#app");
+        builder.RootComponents.Add<HeadOutlet>("head::after");
 
-await BlazorCryptoProvider.InitializeBlazorCryptoProviderAsync();
+        builder.Services.AddScoped(_ => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
-await builder.Build().RunAsync();
+        await BlazorCryptoProvider.InitializeBlazorCryptoProviderAsync();
+
+        await builder.Build().RunAsync();
+    }
+}
