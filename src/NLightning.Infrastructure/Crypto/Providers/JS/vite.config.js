e3320c4c575c55279070a3b3da2b06204795d34b
Níckolas Goline <nickolas.goline+github@gmail.com>
2025-06-16 13:02:04 -0300

    remove js bundle files from the solution and create it while building `NLightning.Infrastructure` for `*.Wasm`
    Signed-off-by: Níckolas Goline <nickolas.goline+github@gmail.com>

---
+++ src/NLightning.Infrastructure/Crypto/Providers/JS/vite.config.js
@@ -0,0 +1,16 @@
+import {defineConfig} from 'vite';
+import {resolve} from 'path';
+
+export default defineConfig({
+    build: {
+        target: 'esnext',
+        minify: 'esbuild',
+        lib: {
+            entry: resolve(__dirname, 'blazorSodium.js'),
+            formats: ['es'],
+            fileName: 'blazorSodium.bundle'
+        },
+        outDir: '../../../wwwroot',
+        sourcemap: false
+    }
+});
